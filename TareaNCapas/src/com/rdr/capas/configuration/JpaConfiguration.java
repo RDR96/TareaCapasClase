235f625435fc595c8992e00214e25afe1e4a1fe0
RDR96 <00231514@uca.edu.sv>
2019-05-22 22:37:22 -0600

    Terminada

--- TareaNCapas/src/com/rdr/capas/configuration/JpaConfiguration.java
+++ TareaNCapas/src/com/rdr/capas/configuration/JpaConfiguration.java
@@ -31,7 +31,7 @@ public class JpaConfiguration {
 	public DataSource dataSource(){
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://127.0.0.1:5433/library_p");
+		dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/library");
 		dataSource.setUsername("postgres");
 		dataSource.setPassword("postgres");
 		return dataSource;
